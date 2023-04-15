709f8cf768d54cf1d01d68b7c33654e1d618ec8a
Christian Berchtold <christian.berchtold@student.supsi.ch>
2023-04-15 12:22:49 +0200

    updated with one season of games

--- retry/loader.py
+++ retry/loader.py
@@ -3,12 +3,14 @@ from pathlib import Path
 
 
 def load_data(path_to_forlder: str):
-    df = pd.concat([pd.read_csv(Path(path_to_forlder, "hockey_15-16.csv"), sep=";"), 
-                    pd.read_csv(Path(path_to_forlder, "hockey_16-17.csv"), sep=";"), 
-                    pd.read_csv(Path(path_to_forlder, "hockey_17-18.csv"), sep=";"), 
-                    pd.read_csv(Path(path_to_forlder, "hockey_18-19.csv"), sep=";"), 
-                    pd.read_csv(Path(path_to_forlder, "hockey_19-20.csv"), sep=";"), 
-                    pd.read_csv(Path(path_to_forlder, "hockey_20-21.csv"), sep=";"),
-                    pd.read_csv(Path(path_to_forlder, "hockey_21-22.csv"), sep=";"),], axis=0, ignore_index=1)
+    df = pd.read_csv(Path(path_to_forlder, "hockey_21-22.csv"), sep=";")
+
     
     return df
+
+# pd.read_csv(Path(path_to_forlder, "hockey_15-16.csv"), sep=";"), 
+# pd.read_csv(Path(path_to_forlder, "hockey_16-17.csv"), sep=";"), 
+# pd.read_csv(Path(path_to_forlder, "hockey_17-18.csv"), sep=";"), 
+# pd.read_csv(Path(path_to_forlder, "hockey_18-19.csv"), sep=";"), 
+# pd.read_csv(Path(path_to_forlder, "hockey_19-20.csv"), sep=";"), 
+# pd.read_csv(Path(path_to_forlder, "hockey_20-21.csv"), sep=";"),
